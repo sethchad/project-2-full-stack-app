44051ea179d79a4ba228690bcf48b79ff8dbd5b2
Chadwick Seth W <seth.chadwick@gmail.com>
2021-02-10 08:53:15 -0600

    Routes for adding and deleting tasks added to taskController

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -14,11 +14,23 @@ router.get("/", (req, res) => {
 	});
 });
 
+
+// NEW
+router.get('/new', (req, res) => {
+	res.render('new.ejs');
+});
+  
+router.post('/', (req, res) => {
+	Task.create(req.body).then(() => {
+		res.redirect('/tasks');
+	})
+});
+
 // DELETE route
 router.delete('/:id', (req, res) => {
 	Task.destroy({ where: {id: req.params.id} }).then(() => {
 	  res.redirect('/tasks');
 	});
-  });
+});
 
 module.exports = router;
